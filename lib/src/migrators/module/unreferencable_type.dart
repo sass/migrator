063b5403fe7c4586d307cebc2b313e70f590b716
Jennifer Thakar <jathak@gmail.com>
2025-07-18 12:37:29 -0700

    Better handling of late imports
    Fixes #278.
    
    Also fixes a bug where a file that uses and imports the same dependency
    would be migrated to duplicate `@use` rules.

--- lib/src/migrators/module/unreferencable_type.dart
+++ lib/src/migrators/module/unreferencable_type.dart
@@ -19,6 +19,9 @@ class UnreferencableType {
   /// For members from a nested import.
   static const fromNestedImport = UnreferencableType._('fromNestedImport');
 
+  /// For members from a late import.
+  static const fromLateImport = UnreferencableType._('fromLateImport');
+
   /// Identifier for this unreferencable type.
   final String id;
 
@@ -36,17 +39,32 @@ class UnreferencableType {
     switch (this) {
       case fromImporter:
         return MigrationSourceSpanException(
-            "This stylesheet was loaded by a nested import in $url. The module "
-            "system only supports loading nested CSS using the load-css() "
-            "mixin, which doesn't allow access to ${type}s from the outer "
-            "stylesheet.",
-            reference.span);
+          "This stylesheet was loaded by a nested import in $url.\n"
+          "The module system only supports loading nested CSS using the "
+          "load-css() mixin,\nwhich doesn't allow access to ${type}s from "
+          "the outer stylesheet.",
+          reference.span,
+        );
       case fromNestedImport:
         return MigrationSourceSpanException(
-            "This $type was loaded from a nested import of $url. The module "
-            "system only supports loading nested CSS using the load-css() "
-            "mixin, which doesn't load ${type}s.",
-            reference.span);
+          "This $type was loaded from a nested import of $url.\n"
+          "The module system only supports loading nested CSS using the "
+          "load-css() mixin,\nwhich doesn't load ${type}s.",
+          reference.span,
+        );
+      case fromLateImport:
+        return MigrationSourceSpanException(
+          "This $type was loaded from a late import of $url,\n"
+          "which also emits CSS. Splitting this stylesheet into one "
+          "containing ${type}s\nand one emitting CSS will allow the migrator "
+          "to safely migrate it by hoisting\nthe one containing members to "
+          "the top of the file and converting the one\nemitting CSS to "
+          "meta.load-css().\n\nAlternatively, pass --force-hoist to force the "
+          "migrator to hoist all\nlate imports containing Sass members, "
+          "which may reorder your CSS.",
+          reference.span,
+        );
+
       default:
         throw StateError('Invalid UnreferencableType');
     }
