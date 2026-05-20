61ea3a26fff9a18943fe8b6fe56bb8ebdb40c1c6
Jennifer Thakar <jathak@gmail.com>
2026-05-20 12:07:24 -0700

    Work around Dart 3.13 breaking node_io

--- lib/src/io.dart
+++ lib/src/io.dart
@@ -4,9 +4,4 @@
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
-// These libraries don't expose *exactly* the same API, but they overlap in all
-// the cases we care about.
-export 'dart:io' if (dart.library.js) 'package:node_io/node_io.dart';
-
-// For cases that aren't covered by `node_io`.
 export 'io/vm.dart' if (dart.library.js) 'io/node.dart';
--- lib/src/io/vm.dart
+++ lib/src/io/vm.dart
@@ -4,6 +4,8 @@
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
+export 'dart:io' show File, Directory, exitCode;
+
 import 'dart:io';
 
 import 'package:file/file.dart';
